70b9f24869c5e84283f2b195fce0fb16a3fbc982
charles <charlesbral78@hotmail.com>
2022-06-18 16:59:09 -0300

    Web API Kotlin com Spring Boot

---
+++ src/test/kotlin/com/eprogramar/bank/AccountControllerTest.kt
@@ -0,0 +1,16 @@
+package com.eprogramar.bank
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class AccountControllerTest {
+
+    @Test
+    fun getAll() {
+    }
+}
