9317e5c6f76659d66b27354387e0b8d391c634de
charles <charlesbral78@hotmail.com>
2022-06-25 14:54:56 -0300

    Refatorando e criando testes unitarios

--- src/test/kotlin/com/eprogramar/bank/AccountControllerTest.kt
+++ src/test/kotlin/com/eprogramar/bank/AccountControllerTest.kt
@@ -1,16 +1,105 @@
 package com.eprogramar.bank
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest
 @AutoConfigureMockMvc
 internal class AccountControllerTest {
 
+    @Autowired lateinit var mockMvc: MockMvc
+
+    @Autowired lateinit var accountRepository: AccountRepository
+
     @Test
     fun getAll() {
+
+        accountRepository.save(Account(name = "Charles Cabral", document = "12345", phone = "11965984900"))
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/accounts"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$").isArray)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].id").isNumber)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].name").isString)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].document").isString)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$[0].phone").isString)
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun findById() {
+
+       val account = accountRepository.save(Account(name = "Charles Cabral", document = "12345", phone = "11965984900"))
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/accounts/${account.id}"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.id").value(account.id))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.name").value(account.name))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.document").value(account.document))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.phone").value(account.phone))
+            .andDo(MockMvcResultHandlers.print())
+    }
+    @Test
+    fun testCreateAccount() {
+        val account = Account(name = "Charles Cabral", document = "12345", phone = "11965984900")
+        val json = ObjectMapper().writeValueAsString(account)
+        accountRepository.deleteAll()
+        mockMvc.perform(MockMvcRequestBuilders.post("/accounts")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(json))
+            .andExpect(MockMvcResultMatchers.status().isCreated)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.name").value(account.name))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.document").value(account.document))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.phone").value(account.phone))
+            .andDo(MockMvcResultHandlers.print())
+
+        Assertions.assertFalse(accountRepository.findAll().isEmpty())
+   }
+
+    @Test
+    fun testUpdateAccount() {
+        val account = accountRepository
+            .save(Account(name = "Charles Cabral", document = "12345", phone = "11965984900"))
+            .copy(name = "updated")
+        val json = ObjectMapper().writeValueAsString(account)
+        mockMvc.perform(MockMvcRequestBuilders.put("/accounts/${account.id}")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(json))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.name").value(account.name))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.document").value(account.document))
+            .andExpect(MockMvcResultMatchers.jsonPath("\$.phone").value(account.phone))
+            .andDo(MockMvcResultHandlers.print())
+
+        val findById = accountRepository.findById(account.id!!)
+        Assertions.assertTrue(findById.isPresent)
+        Assertions.assertEquals(account.name, findById.get().name)
+    }
+
+    @Test
+    fun testDeleteAccount() {
+        val account = accountRepository
+            .save(Account(name = "Charles Cabral", document = "12345", phone = "11965984900"))
+        mockMvc.perform(MockMvcRequestBuilders.delete("/accounts/${account.id}"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andDo(MockMvcResultHandlers.print())
+
+        val findById = accountRepository.findById(account.id!!)
+        Assertions.assertFalse(findById.isPresent)
     }
 }
+
+
