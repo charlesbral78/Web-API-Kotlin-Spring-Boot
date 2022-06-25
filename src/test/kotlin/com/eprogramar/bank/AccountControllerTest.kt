8babad7643ec7a64dcb634e81bb22ebf96da6e2d
charles <charlesbral78@hotmail.com>
2022-06-25 16:20:15 -0300

    Organizando em camadas

--- src/test/kotlin/com/eprogramar/bank/AccountControllerTest.kt
+++ src/test/kotlin/com/eprogramar/bank/AccountControllerTest.kt
@@ -1,10 +1,11 @@
 package com.eprogramar.bank
 
+import com.eprogramar.bank.model.Account
+import com.eprogramar.bank.repository.AccountRepository
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
